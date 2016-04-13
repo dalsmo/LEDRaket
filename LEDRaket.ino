17c7da542f358ee8901879193691adf0f2a4d68d
dalsmo <adalsmo@gmail.com>
2016-04-13 21:51:14 +0200

    Update LEDRaket.ino
    temoprary fixed so that it works with raspbery pie compiler

--- LEDRaket.ino
+++ LEDRaket.ino
@@ -84,7 +84,7 @@ void loop() {
   // kolla om vi skall bli triggade
   if (Serial.available() >0){
     byte inData[12];
-    Serial.readBytes(inData,12);
+    //Serial.readBytes(inData,12);
     Serial.flush();
     raket_laddad = true;
   }
